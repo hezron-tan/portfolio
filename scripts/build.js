7b82e58dceed4a7a4558c3511258ff681e778d92
Hezron Tan <htan@planittesting.com>
2026-06-15 14:12:06 +1200

    Add portfolio data JSON and update script to load skills and experiences dynamically

--- scripts/build.js
+++ scripts/build.js
@@ -5,6 +5,7 @@ const distDir = path.resolve(__dirname, "..", "dist");
 const assetDir = path.join(distDir, "assets");
 const cssDir = path.join(assetDir, "css");
 const jsDir = path.join(assetDir, "js");
+const dataDir = path.join(assetDir, "data");
 
 const copyFile = (source, destination) => {
   fs.mkdirSync(path.dirname(destination), { recursive: true });
@@ -14,6 +15,7 @@ const copyFile = (source, destination) => {
 const filesToCopy = [
   { src: path.resolve(__dirname, "..", "index.html"), dest: path.join(distDir, "index.html") },
   { src: path.resolve(__dirname, "..", "assets", "css", "styles.css"), dest: path.join(cssDir, "styles.css") },
+  { src: path.resolve(__dirname, "..", "assets", "data", "portfolio-data.json"), dest: path.join(dataDir, "portfolio-data.json") },
   { src: path.resolve(__dirname, "..", "assets", "js", "script.js"), dest: path.join(jsDir, "script.js") },
 ];
 
